613a25d495c188be809b0bd958ee584ba4ccf342
Domokos Molnar <domokos@gmail.com>
2015-01-06 18:43:17 +0100

    Fine tune log

--- Bus_comm/boiler_controller.rb
+++ Bus_comm/boiler_controller.rb
@@ -816,7 +816,7 @@ class Heating_State_Machine
     $heating_logger.debug("Active state: "+@state.name.to_s)
 
     sth=""
-    @state_history.each {|e| sth+= ") => ("+e[:state].to_s+","+e[:power].to_s+", "+(Time.now.to_i-e[:timestamp]).to_s+" sec ago"}
+    @state_history.each {|e| sth+= ") => ("+e[:state].to_s+","+e[:power].to_s+", "+(Time.now.to_i-e[:timestamp]).to_s+"s ago"}
     $heating_logger.debug("State and power_needed history : "+sth[5,1000]+")")
     $heating_logger.debug("Forward temperature: "+@forward_temp.round(2).to_s)
     $heating_logger.debug("Return water temperature: "+@return_temp.round(2).to_s)
