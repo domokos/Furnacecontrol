cecb7343237b4bfb081c2af48e6e39f23ddc8cc5
doma <doma@localhost>
2016-01-14 23:01:03 +0100

    Fix valves and pumps control

--- bin/boiler_controller.rb
+++ bin/boiler_controller.rb
@@ -564,7 +564,8 @@ class Heating_controller
   # This function controls valves, pumps and heat during heating by evaluating the required power
   def control_pumps_valves_for_heating(prev_power_needed,power_needed)
 
-    changed = prev_power_needed[:power] != power_needed[:power]
+    changed = prev_power_needed[:power] != power_needed[:power] or \
+    prev_power_needed[:power] == :off
 
     $app_logger.trace("Setting valves and pumps")
 
